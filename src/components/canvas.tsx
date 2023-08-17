d54bebd014e4fb0fa305bbeb381d08e71ca21551
zwe <zwezeya02@gmail.com>
2023-08-17 23:05:52 +0800

    add event handler

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,7 +1,34 @@
-const Canvas = () => {
-    return (
-        <div>
+import { useState } from "react";
 
+interface Point {
+    x: number,
+    y: number
+}
+
+const Canvas = () => {
+    const [points, setPoint] = useState<Point[]>([]);
+
+    const handleClick = (e) => {
+        setPoint([...points, { x: e.clientX, y: e.clientY }]);
+    }
+
+    return (
+        <div
+            onClick={handleClick}
+            style={{
+                height: '100vh',
+                width: '100vw'
+            }}
+        >
+            {
+                points.map(p => <div 
+                    style={{
+                        position: 'absolute',
+                        top: p.y + 'px',
+                        left: p.x + 'px'
+                    }}
+                    >o</div>)
+            }
         </div>
     )
 }
